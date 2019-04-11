eb40143c35f205cec532717d8a803d8c8a4acc64
Владимир <vovakalini4ev@gmail.com>
2019-04-11 16:28:55 +0300

    Добавьте файлы проекта.

---
+++ Task_02/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace TaskTwo
+{
+
+
+    /// Есть базовый класс с реалиазцией двух интерфейсов, имеющих одинаковый метод
+    /// строка var @base = new Base(); - создаёт объект типа Base
+    /// вызов @base.WriteExecutor()    - выводит на экран строку I base Executor!
+    ///
+    /// Дополните код так, чтобы не создавая новый объект на экран было выведено
+    ///
+    /// I base Executor!
+    /// I one Executor!
+    /// I two Executor!
+
+    interface IOneExecutor
+    {
+        void WriteExecutor();
+        
+    }
+    
+    interface ITwoExecutor
+    {
+        void WriteExecutor();
+    }
+
+    class Base : IOneExecutor, ITwoExecutor
+    {
+        public void WriteExecutor()
+        {
+            Console.WriteLine("I base Executor!");
+        }
+        void IOneExecutor.WriteExecutor()
+        {
+            Console.WriteLine("I One Executor!");
+        }
+        void ITwoExecutor.WriteExecutor()
+        {
+            Console.WriteLine("I Two Executor!");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var @base = new Base();
+            IOneExecutor interface1 = @base;
+            ITwoExecutor interface2 = @base;
+            @base.WriteExecutor();
+            interface1.WriteExecutor();
+            interface2.WriteExecutor();
+            Console.ReadKey();
+           
+
+        }
+    }
+}
